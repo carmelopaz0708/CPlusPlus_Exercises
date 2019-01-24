cc659b11e0fac17d6acf78fb8199328bd8c3ea8a
Carmelo 'Siege' Paz <carmelopaz0708@gmail.com>
2019-01-24 18:34:52 +0800

    Create exer_01.cpp

---
+++ exer_01.cpp
@@ -0,0 +1,12 @@
+/*
+  exer_01.cpp
+  A C++ program that writes a welcome text in a separate line.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "\nHello, world!" << endl;
+}
