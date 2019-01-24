1a55dcefd3b719fe53186764d7f7a0fbc34b6a2e
Carmelo 'Siege' Paz <carmelopaz0708@gmail.com>
2019-01-24 19:17:59 +0800

    Update and rename exer_02.cpp to exer_04.cpp

--- exer_04.cpp
+++ exer_04.cpp
@@ -1,6 +1,6 @@
 /*
-  exer_02.cpp
-  A C++ program that prints the sum of two numbers.
+  exer_04.cpp
+  A C++ program that prints the sum of two numbers using variables.
 */
 
 #include <iostream>
@@ -10,6 +10,7 @@ using namespace std;
 int main() {
     int num1;
     int num2;
+    cout << "--------------------------------------------";
     cout << "Enter first number: ";
     cin >> num1;
     cout << "Enter second number: ";
