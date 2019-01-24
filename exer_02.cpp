8c3c614e0bb959d9b0da43a898c4992c144d5a41
Carmelo 'Siege' Paz <carmelopaz0708@gmail.com>
2019-01-24 18:42:46 +0800

    Create exer_02.cpp

---
+++ exer_02.cpp
@@ -0,0 +1,18 @@
+/*
+  exer_02.cpp
+  A C++ program that prints the sum of two numbers.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1;
+    int num2;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    cout << num1 << " + " << num2 << " = " << num1 + num2;
+}
