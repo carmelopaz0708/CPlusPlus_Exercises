7baa897bf6a418efd3b83a2dd4b7101851ad9ec2
Carmelo 'Siege' Paz <carmelopaz0708@gmail.com>
2019-01-24 20:00:21 +0800

    Create exer_05.cpp

---
+++ exer_05.cpp
@@ -0,0 +1,27 @@
+/*
+  exer_04.cpp
+  A C++ program that prints the minimum and maximum sizes for data types.
+*/
+
+#include <iostream>
+#include <climits>      // Standard library for defining data type sizes
+
+using namespace std;
+
+int main() {
+    cout << "The maximum limit of \'int\' data type: " << INT_MAX << endl;
+    cout << "The minimum limit of \'int\' data type: " << INT_MIN << endl;
+    cout << "The maximum limit of \'unsigned int\' data type: " << UINT_MAX << endl;
+    cout << "The maximum limit of \'long long\' data type: " << LLONG_MAX << endl;
+    cout << "The minimum limit of \'long long\' data type: " << LLONG_MIN << endl;
+    cout << "The maximum limit of \'unsigned long long\' data type: " << ULLONG_MAX << endl;
+    cout << "The Bits contain in \'char\' data type: " << CHAR_BIT << endl;
+    cout << "The maximum limit of \'char\' data type: " << CHAR_MAX << endl;
+    cout << "The minimum limit of \'char\' data type: " << CHAR_MIN << endl;
+    cout << "The maximum limit of \'signed char\' data type: " << SCHAR_MAX << endl;
+    cout << "The minimum limit of \'signed char\' data type: " << SCHAR_MIN << endl;
+    cout << "The maximum limit of \'unsigned char\' data type: " << UCHAR_MAX << endl;
+    cout << "The minimum limit of \'short\' data type: " << SHRT_MIN << endl;
+    cout << "The maximum limit of \'short\' data type: " << SHRT_MAX << endl;
+    cout << "The maximum limit of \'unsigned short\' data type: " << USHRT_MAX << endl;
+}
